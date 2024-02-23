ad6fcf9751e80f4d901be26ffb530fd37dde4ddd
Collinsk70 <collinskarweru@gmail.com>
2024-02-23 10:57:44 -0800

    commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install puppet-lint -v 2.5.0
+# Install Flask version 2.1.0 using pip3
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
 }
+
